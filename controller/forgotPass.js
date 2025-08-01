a747e1bcd4a57d1dbbf6a96a80a7ee8ed9c95701
chirag_k_poojary <chiragkpoojary30@gmail.com>
2025-08-01 23:18:28 +0530

    NodeMailer implement for password reset

--- controller/forgotPass.js
+++ controller/forgotPass.js
@@ -32,7 +32,10 @@ async function forgotPass(req, res) {
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
       Please click on the following link, or paste this into your browser to complete the process:\n\n
       ${url}\n\n
-      If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+      If you did not request this, please ignore this email and your password will remain unchanged.\n\n
+      your's sincerly,
+      nestnodes
+      `,
     };
 
     await transpoder.sendMail(mailOptions);
