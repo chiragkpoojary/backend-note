7b1556ef7c4a8e731e4d9665f387c9bc8fca6131
chirag_k_poojary <chiragkpoojary30@gmail.com>
2025-08-09 12:54:50 +0530

    cors updated for cnote

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
  import  connectdb from "./database/connect.js"; 
 
 app.use(express.json());
-app.use(cors({origin: 'cnote.vercel.app',
+app.use(cors({origin: 'https://cnote.vercel.app',
     methods: ['GET', 'POST','DELETE']}));
 app.use("/api",changepass_router);
 app.use("/api",search_router);
