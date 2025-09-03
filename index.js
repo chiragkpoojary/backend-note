53f95a065495f17ba494063fe0ac172ee29fa68c
chirag_k_poojary <chiragkpoojary30@gmail.com>
2025-09-03 20:18:59 +0530

    testing for ci/cd pipeline

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ import polling from "./router/polling.js";
 app.use("/api",routers_per_del)
 app.use("/api",polling);
 app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'okkk1' });
+    res.status(200).json({ status: 'ok' });
   });
   
 const PORT = process.env.PORT || 8080;
